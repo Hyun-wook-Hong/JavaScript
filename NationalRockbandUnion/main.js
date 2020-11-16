24a630f1af6fd9aa30f3dfb79dff53410ae3725d
Jeremy Hong <61657215+Hyun-wook-Hong@users.noreply.github.com>
2020-11-16 11:03:20 +0900

    Add files via upload

--- NationalRockbandUnion/main.js
+++ NationalRockbandUnion/main.js
@@ -10,20 +10,6 @@
 //main.js
 "use strict";
 
-console.log("Mayday test.");
-
-// 아직 Contents가 없어서 스크롤이 안내려가서 null로 잡힘
-
-// Navbar tag scroll event
-
-const navbar = document.querySelector('#navbar');
+// navbar hide/hover 변수 선언
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
-document.addEventListener('scroll', ()=>{
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
-
-    if(window.scrollY > navbarHeight){
-        navbar.classList.add('navbar--dark');
-    }else { navbar.classList.remove('navbar--dark')}
-})
